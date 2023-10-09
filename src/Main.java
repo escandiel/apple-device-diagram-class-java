e0595229d33aa131ff952ba7715663d988f9f3d4
Victor Escandiel <joaoescandielb@gmail.com>
2023-10-09 16:16:48 -0300

    new Class: Main; Running the methods

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,13 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        // Criar um objeto Iphone
+        Iphone iphone = new Iphone();
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        // Chamar os métodos diretamente do objeto iphone
+        iphone.ligar();
+        iphone.reproduzirMusica();
+        iphone.fazerChamada("123456789");
+        iphone.abrirNavegador();
+        iphone.desligar();
     }
 }
